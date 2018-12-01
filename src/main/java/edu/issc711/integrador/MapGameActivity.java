884a57f1094b50fdf6fd8698bf93f6afe5d3f2b1
57LECS <eddsc57@gmail.com>
2018-11-30 22:49:11 -0600

    Added Game map activity

---
+++ src/main/java/edu/issc711/integrador/MapGameActivity.java
@@ -0,0 +1,13 @@
+package edu.issc711.integrador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_game);
+    }
+}
