8aaa707027422f7f0cd9a8a7fda2dbed7056c436
Axel Dávalos Romo <axeldavalos97@gmail.com>
2018-12-01 01:55:28 -0600

    Activity Base Login

---
+++ src/main/java/edu/issc711/integrador/LoginActivity.java
@@ -0,0 +1,13 @@
+package edu.issc711.integrador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
