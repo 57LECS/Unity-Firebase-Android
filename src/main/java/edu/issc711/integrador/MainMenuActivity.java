e4a8f56f15951fd3189d4009f48a668bb5eb03a3
Axel Dávalos Romo <axeldavalos97@gmail.com>
2018-12-01 20:03:13 -0600

    Pantalla principal con navegacion a highscores

--- src/main/java/edu/issc711/integrador/MainMenuActivity.java
+++ src/main/java/edu/issc711/integrador/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package edu.issc711.integrador;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnHighscores)
+    public void goToHighscores(){
+        Intent intent = new Intent( this, HighScoresActivity.class );
+        startActivity(intent);
     }
 }
