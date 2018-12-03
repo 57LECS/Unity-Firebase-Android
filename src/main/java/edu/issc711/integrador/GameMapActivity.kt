47257ae70b1959376ce9d545106b145811f65466
57LECS <eddsc57@gmail.com>
2018-12-03 13:53:58 -0600

    Almost done app

---
+++ src/main/java/edu/issc711/integrador/GameMapActivity.kt
@@ -0,0 +1,12 @@
+package edu.issc711.integrador.Models
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_map)
+    }
+}
---
+++ src/main/java/edu/issc711/integrador/Models/Pregunta.kt
@@ -0,0 +1,7 @@
+package edu.issc711.integrador.Models
+
+/**
+ * Created by Luis Calderon on 03/12/2018.
+ */
+class Pregunta {
+}
